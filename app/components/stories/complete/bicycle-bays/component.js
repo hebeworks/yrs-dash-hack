44e25448616b5b7eff7bba6569dee8c1fe50bed6
Nathan Smith <nathan@hebemedia.com>
2015-09-14 11:14:37 +0100

    WIP

--- app/components/stories/complete/bicycle-bays/component.js
+++ app/components/stories/complete/bicycle-bays/component.js
@@ -16,7 +16,7 @@ export default DatamillStory.extend({
 
     didInsertElement: function () {
         this.set('title', 'Bicycle Bays');
-        this.set('subTitle', 'In the city centre');
+        this.set('subTitle', 'Directions');
         var obj = this;
 
         this.getData(this.get('datamillUrl') + '/api/action/datastore_search?resource_id=c2bb0c3e-52fd-4183-8727-6b9f40b829f0')
